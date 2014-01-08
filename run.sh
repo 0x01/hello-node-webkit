9be0294d50c9d4c1dbda7571af11898af70709f1
wires <jelle@defekt.nl>
2014-01-08 02:04:12 +0100

    fullscreen, lalz

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 # run using https://github.com/rogerwang/node-webkit/
-zip app.nw index.html package.json ; open -a node-webkit app.nw
+zip app.nw grid.png index.html package.json ; open -a node-webkit app.nw
